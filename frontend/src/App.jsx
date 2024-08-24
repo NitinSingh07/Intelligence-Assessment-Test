634a9a65b4f04899de2831035e690375859ef434
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2024-08-24 16:29:08 +0530

    chore: Update route path for home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
